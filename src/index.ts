9795bcbd8fef9a2d80cef396af8e376f6da7d2d2
Tobik <klaba20102@zak.gvid.cz>
2025-05-30 20:46:07 +0200

    ver2

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Hono } from 'hono'
 import { GoogleGenAI } from '@google/genai';
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 
-const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
+const ai = new GoogleGenAI({ apiKey: "AIzaSyD9maWTSpB87rv5KMdkVdJSlPnpYfUUqLU" });
 const app = new Hono()
 
 
