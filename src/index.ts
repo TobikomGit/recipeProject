2ec47a36fea53c2763c397a776a0bbf756ac550d
Tobik <klaba20102@zak.gvid.cz>
2025-05-31 22:14:03 +0200

    prints Test, not the File

--- src/index.ts
+++ src/index.ts
@@ -17,11 +17,43 @@ app.get('/api/getrecipe', async (c) => {
 async function fce() {
   const response = await ai.models.generateContent({
     model: 'gemini-2.0-flash-001',
-    contents: 'Why is the sky blue?',
+    contents: 'Napiš mi prosím recept podle těchto údajů:' + "Pouze z těchto ingrediencí:" + ingredience + "Maximálně o této délce vaření:" + test.cookTime + "Má být pokrm studený:" + test.isCold,
   });
-  console.log(response.text);
+  //console.log(response.text);
 
   return response.text
 }
+
+interface cookData {
+  ingredients: ingredient[],
+  cookTime: String,
+  isCold: Boolean,
+}
+
+interface ingredient {
+  name: String,
+  count: String,
+}
+
+const test = {
+  ingredients: [
+    { name: "brambory", count: "4" },
+    { name: "brambory", count: "4" },
+    { name: "brambory", count: "4" },
+    { name: "brambory", count: "4" },
+    { name: "brambory", count: "4" },
+  ],
+  cookTime: '30 min',
+  isCold: false,
+}
+
+const ingredience: string | string[] = []
+for (let x of test.ingredients) {
+  ingredience.push(x.name + ", " + x.count)
+}
+console.log(ingredience)
+console.log(test.cookTime)
+console.log(test.isCold)
+
 
 export default app
